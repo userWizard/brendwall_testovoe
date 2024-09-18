93437307fb2ceb8994b5fb9d8abc9d3870aa400b
ragecodemode <nikitaloskutov18@gmail.com>
2024-09-18 09:44:48 +0300

    Nothing

--- core/project/settings/main.py
+++ core/project/settings/main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import environ
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 env = environ.Env()
 environ.Env.read_env(BASE_DIR / '.env')
 
